7448c70bdef7a462ac542d01b0ddb8602abf2ea2
Lothar Haeger <lothar.haeger@brummelhook.com>
2015-12-12 17:53:31 +0100

    Added MPL comments

--- Mac/ValidatorLauncher for Mac/AppDelegate.swift
+++ Mac/ValidatorLauncher for Mac/AppDelegate.swift
@@ -5,6 +5,9 @@
 //  Created by Lothar Haeger on 03.09.15.
 //  Copyright (c) 2015 Lothar Haeger. All rights reserved.
 //
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import CoreServices
 import Cocoa
--- Mac/ValidatorLauncher for Mac/StreamReader.swift
+++ Mac/ValidatorLauncher for Mac/StreamReader.swift
@@ -5,6 +5,10 @@
 //  Created by Lothar Haeger on 05.09.15.
 //  Copyright (c) 2015 Lothar Haeger. All rights reserved.
 //
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 
 import Foundation
 
